57a70faca94a8acb49182495f2648c27cbad9091
Claudio López Ardura <cllopez@aidacanarias.com>
2021-11-15 15:17:22 +0100

    Added test case for number 2

--- StringCalculatorKata/StringCalculatorSpec.cs
+++ StringCalculatorKata/StringCalculatorSpec.cs
@@ -19,5 +19,13 @@ namespace StringCalculatorKata
             int result = StringCalculator.Add("1");
             Assert.Equal(expectedValue, result);
         }
+
+        [Fact]
+        public void Return2WhenIsTheNumber2AsString()
+        {
+            int expectedValue = 2;
+            int result = StringCalculator.Add("2");
+            Assert.Equal(expectedValue, result);
+        }
     }
 }
